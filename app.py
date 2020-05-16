3c9eeb2287d6826202392d13e5c1fc999ff5f79f
Rodrigo Pires <47892386+palatam@users.noreply.github.com>
2020-05-15 23:31:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import dash_html_components as html
 import time
 from datetime import datetime as dt
 import pandas as pd
-import psycopg2
 import plotly.graph_objs as go
 import re
 pd.set_option('display.max_rows', 500)
@@ -55,144 +54,6 @@ def get_options(df, df_column):
 
 app = dash.Dash(__name__)
 server = app.server
-
-## -- CALLBACKS --
-
-@app.callback([Output('graph-output1', 'children'),
-                Output('graph-output2', 'children'),
-                Output('graph-output3', 'children'),
-                Output('graph-output4', 'children'),
-                Output('table-output1', 'children')],
-                [Input('classificacao_multiselect', 'value'),
-                Input('tipo_ocorrencia_multiselect', 'value'),
-                Input('area_fator_contribuinte_multiselect', 'value'),
-                Input('fator_contribuinte_multiselect', 'value'),
-                Input('date-range-picker', 'start_date'),
-                Input('date-range-picker', 'end_date'),
-                Input('campos_relatorio_multiselect', 'value')])
-def generate_objects(classificacao_value, tipo_ocorrencia_value, area_fator_contribuinte_value, fator_contribuinte_value, start_date, end_date, campos_relatorio_value, max_rows=10):
-
-    df = get_data()
-
-    ## FILTROS
-    if classificacao_value != None and len(classificacao_value) != 0:
-        df = df[df['ocorrencia_classificacao'].isin(classificacao_value)]
-    if tipo_ocorrencia_value != None and len(tipo_ocorrencia_value) != 0:
-        df = df[df['ocorrencia_tipo'].isin(tipo_ocorrencia_value)]
-    if area_fator_contribuinte_value != None and len(area_fator_contribuinte_value) != 0:
-        df = df[df['fator_area'].isin(area_fator_contribuinte_value)]
-    if fator_contribuinte_value != None and len(fator_contribuinte_value) != 0:
-        df = df[df['fator_nome'].isin(fator_contribuinte_value)]
-    if start_date != None and len(start_date) != 0 and end_date != None and len(end_date) != 0:
-        print(type(start_date))
-        df = df[(df['ocorrencia_dia'].astype('datetime64[ns]')>start_date) & (df['ocorrencia_dia'].astype('datetime64[ns]')<end_date)]
-    else:
-        df = df
-
-    ## GRÁFICOS (TAB 1)
-    graph_width = 55
-    graph_height = 32
-
-    # Gráfico 1
-    temp = df.groupby('ocorrencia_classificacao')
-    piedata = go.Pie(labels=list(temp.groups.keys()),values=temp['codigo_ocorrencia'].nunique().tolist())
-    Graph1 = dcc.Graph(
-            figure={
-                'data': [piedata],
-                'layout': {
-                    'title': 'Quantidade Ocorrências por Classificação',
-                    'plot_bgcolor': plot_bgcolor,
-                    'paper_bgcolor': graph_bgcolor,
-                    'display': 'inline-block',
-                    'margin': {
-                        't': 40
-                    }
-                }
-            },
-            style={'width': '{}rem'.format(graph_width), 'height': '{}rem'.format(graph_height)}
-        )
-
-    # Gráfico 2
-    temp = df[['codigo_ocorrencia', 'ocorrencia_ano', 'aeronave_fatalidades_total']].drop_duplicates().groupby('ocorrencia_ano')
-    Graph2 = dcc.Graph(
-            figure={
-                'data': [
-                    {'x': list(temp['codigo_ocorrencia'].nunique().keys()), 'y': temp['codigo_ocorrencia'].nunique().tolist(), 'type': 'bar', 'name': 'Quantidade Ocorrências'},
-                    {'x': list(temp['aeronave_fatalidades_total'].sum().keys()), 'y': temp['aeronave_fatalidades_total'].sum().tolist(), 'type': 'bar', 'name': 'Quantidade Fatalidades'}
-                ],
-                'layout': {
-                    'title': 'Quantidade Ocorrências x Fatalidades por Ano',
-                    'plot_bgcolor': plot_bgcolor,
-                    'paper_bgcolor': graph_bgcolor,
-                    'display': 'inline-block',
-                    'margin': {
-                        't': 40
-                    }
-                },
-
-            },
-            style={'width': '{}rem'.format(graph_width), 'height': '{}rem'.format(graph_height)}
-        )
-
-    # Gráfico 3
-    temp = df[['codigo_ocorrencia', 'fator_area']][df['fator_area']!='***'].groupby('fator_area')
-    Graph3 = dcc.Graph(
-            figure={
-                'data': [go.Bar(x=temp['codigo_ocorrencia'].nunique().sort_values().tolist(),
-                        y=list(temp['codigo_ocorrencia'].nunique().sort_values().keys()),
-                        orientation='h')],
-                'layout': {
-                    'title': 'Quantidade Ocorrências por área de fator contribuinte',
-                    'plot_bgcolor': plot_bgcolor,
-                    'paper_bgcolor': graph_bgcolor,
-                    'display': 'inline-block',
-                    'margin': {
-                        't': 30,
-                        'l': 175
-                    }
-                }
-            },
-            style={'width': '{}rem'.format(graph_width), 'height': '{}rem'.format(graph_height)}
-        )
-
-    # Gráfico 4
-    rows_to_show = 10
-    temp = df.groupby('fator_nome')
-    Graph4 = dcc.Graph(
-            figure={
-                'data': [go.Bar(x=temp['codigo_ocorrencia'].nunique().sort_values().tolist()[-1*max_rows:],
-                        y=list(temp['codigo_ocorrencia'].nunique().sort_values().keys())[-1*max_rows:],
-                        orientation='h')],
-                'layout': {
-                    'title': 'Quantidade Ocorrências por fator contribuinte',
-                    'plot_bgcolor': plot_bgcolor,
-                    'paper_bgcolor': graph_bgcolor,
-                    'display': 'inline-block',
-                    'margin': {
-                        't': 30,
-                        'l': 200
-                    }
-                }
-            },
-            style={'width': '{}rem'.format(graph_width), 'height': '{}rem'.format(graph_height)}
-        )
-
-
-    ## TABLES (TAB 2)
-    # temp = df[['codigo_ocorrencia', 'ocorrencia_dia', 'ocorrencia_classificacao', 'fator_area', 'fator_nome']].drop_duplicates()
-    temp = df[campos_relatorio_value].drop_duplicates()
-    Table1 = dash_table.DataTable(
-            columns=[{'name': i, 'id': i} for i in temp.columns.values],
-            style_table={'height': '50rem', 'width': '110rem', 'overflowY': 'auto'},
-            sort_action='native',
-            sort_mode='multi',
-            filter_action='native',
-            export_format='csv',
-            export_headers='ids',
-            data=temp.to_dict('rows')
-    )
-
-    return [Graph1, Graph2, Graph3, Graph4, Table1]
 
 
 app.layout = html.Div([
@@ -338,6 +199,150 @@ app.layout = html.Div([
     ])
     ## END MAIN PANEL
 ])
+
+
+
+
+## -- CALLBACKS --
+
+@app.callback([Output('graph-output1', 'children'),
+                Output('graph-output2', 'children'),
+                Output('graph-output3', 'children'),
+                Output('graph-output4', 'children'),
+                Output('table-output1', 'children')],
+                [Input('classificacao_multiselect', 'value'),
+                Input('tipo_ocorrencia_multiselect', 'value'),
+                Input('area_fator_contribuinte_multiselect', 'value'),
+                Input('fator_contribuinte_multiselect', 'value'),
+                Input('date-range-picker', 'start_date'),
+                Input('date-range-picker', 'end_date'),
+                Input('campos_relatorio_multiselect', 'value')])
+def generate_objects(classificacao_value, tipo_ocorrencia_value, area_fator_contribuinte_value, fator_contribuinte_value, start_date, end_date, campos_relatorio_value, max_rows=10):
+
+    df = get_data()
+
+    ## FILTROS
+    if classificacao_value != None and len(classificacao_value) != 0:
+        df = df[df['ocorrencia_classificacao'].isin(classificacao_value)]
+    if tipo_ocorrencia_value != None and len(tipo_ocorrencia_value) != 0:
+        df = df[df['ocorrencia_tipo'].isin(tipo_ocorrencia_value)]
+    if area_fator_contribuinte_value != None and len(area_fator_contribuinte_value) != 0:
+        df = df[df['fator_area'].isin(area_fator_contribuinte_value)]
+    if fator_contribuinte_value != None and len(fator_contribuinte_value) != 0:
+        df = df[df['fator_nome'].isin(fator_contribuinte_value)]
+    if start_date != None and len(start_date) != 0 and end_date != None and len(end_date) != 0:
+        print(type(start_date))
+        df = df[(df['ocorrencia_dia'].astype('datetime64[ns]')>start_date) & (df['ocorrencia_dia'].astype('datetime64[ns]')<end_date)]
+    else:
+        df = df
+
+    ## GRÁFICOS (TAB 1)
+    graph_width = 55
+    graph_height = 32
+
+    # Gráfico 1
+    temp = df.groupby('ocorrencia_classificacao')
+    piedata = go.Pie(labels=list(temp.groups.keys()),values=temp['codigo_ocorrencia'].nunique().tolist())
+    Graph1 = dcc.Graph(
+            figure={
+                'data': [piedata],
+                'layout': {
+                    'title': 'Quantidade Ocorrências por Classificação',
+                    'plot_bgcolor': plot_bgcolor,
+                    'paper_bgcolor': graph_bgcolor,
+                    'display': 'inline-block',
+                    'margin': {
+                        't': 40
+                    }
+                }
+            },
+            style={'width': '{}rem'.format(graph_width), 'height': '{}rem'.format(graph_height)}
+        )
+
+    # Gráfico 2
+    temp = df[['codigo_ocorrencia', 'ocorrencia_ano', 'aeronave_fatalidades_total']].drop_duplicates().groupby('ocorrencia_ano')
+    Graph2 = dcc.Graph(
+            figure={
+                'data': [
+                    {'x': list(temp['codigo_ocorrencia'].nunique().keys()), 'y': temp['codigo_ocorrencia'].nunique().tolist(), 'type': 'bar', 'name': 'Quantidade Ocorrências'},
+                    {'x': list(temp['aeronave_fatalidades_total'].sum().keys()), 'y': temp['aeronave_fatalidades_total'].sum().tolist(), 'type': 'bar', 'name': 'Quantidade Fatalidades'}
+                ],
+                'layout': {
+                    'title': 'Quantidade Ocorrências x Fatalidades por Ano',
+                    'plot_bgcolor': plot_bgcolor,
+                    'paper_bgcolor': graph_bgcolor,
+                    'display': 'inline-block',
+                    'margin': {
+                        't': 40
+                    }
+                },
+
+            },
+            style={'width': '{}rem'.format(graph_width), 'height': '{}rem'.format(graph_height)}
+        )
+
+    # Gráfico 3
+    temp = df[['codigo_ocorrencia', 'fator_area']][df['fator_area']!='***'].groupby('fator_area')
+    Graph3 = dcc.Graph(
+            figure={
+                'data': [go.Bar(x=temp['codigo_ocorrencia'].nunique().sort_values().tolist(),
+                        y=list(temp['codigo_ocorrencia'].nunique().sort_values().keys()),
+                        orientation='h')],
+                'layout': {
+                    'title': 'Quantidade Ocorrências por área de fator contribuinte',
+                    'plot_bgcolor': plot_bgcolor,
+                    'paper_bgcolor': graph_bgcolor,
+                    'display': 'inline-block',
+                    'margin': {
+                        't': 30,
+                        'l': 175
+                    }
+                }
+            },
+            style={'width': '{}rem'.format(graph_width), 'height': '{}rem'.format(graph_height)}
+        )
+
+    # Gráfico 4
+    rows_to_show = 10
+    temp = df.groupby('fator_nome')
+    Graph4 = dcc.Graph(
+            figure={
+                'data': [go.Bar(x=temp['codigo_ocorrencia'].nunique().sort_values().tolist()[-1*max_rows:],
+                        y=list(temp['codigo_ocorrencia'].nunique().sort_values().keys())[-1*max_rows:],
+                        orientation='h')],
+                'layout': {
+                    'title': 'Top 10 Quantidade Ocorrências por fator contribuinte',
+                    'plot_bgcolor': plot_bgcolor,
+                    'paper_bgcolor': graph_bgcolor,
+                    'display': 'inline-block',
+                    'margin': {
+                        't': 30,
+                        'l': 200
+                    }
+                }
+            },
+            style={'width': '{}rem'.format(graph_width), 'height': '{}rem'.format(graph_height)}
+        )
+
+
+    ## TABLES (TAB 2)
+    # temp = df[['codigo_ocorrencia', 'ocorrencia_dia', 'ocorrencia_classificacao', 'fator_area', 'fator_nome']].drop_duplicates()
+    temp = df[campos_relatorio_value].drop_duplicates()
+    Table1 = dash_table.DataTable(
+            columns=[{'name': i, 'id': i} for i in temp.columns.values],
+            style_table={'height': '50rem', 'width': '110rem', 'overflowY': 'auto'},
+            sort_action='native',
+            sort_mode='multi',
+            filter_action='native',
+            export_format='csv',
+            export_headers='ids',
+            data=temp.to_dict('rows')
+    )
+
+    return [Graph1, Graph2, Graph3, Graph4, Table1]
+
+
+
 
 if __name__ == '__main__':
     app.run_server()
